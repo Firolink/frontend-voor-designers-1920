cb381a54af3975639d5107fcfa0eb8dca2824a67
Firolink <firolink@live.nl>
2020-04-08 17:08:39 +0200

    final update

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -1,8 +1,7 @@
-Door verkeerd inplannen van mijn andere opleveringen heb ik niet veel verandering nuu, maar daar ga ik dit weekend mee verder.
 
 # frontend voor designers - opdracht 3: Een interactie uitwerken met externe data
 
-mijn plan is om een carousel van films te maken die ik dan daaruit kan slepen in een dropbox, en ik wil de dropbox in en uit het scherm kunnen laten komen.
+mijn plan is om een carrousel van films te maken die ik dan daaruit kan slepen in een dropbox, en ik wil de dropbox in en uit het scherm kunnen laten komen.
 
 ## stap 1: Schetsen
 
@@ -13,11 +12,11 @@ Mijn idee is om een carousel plugin en de Sortable plugin samen te gebruiken zod
 
 ## stap 2: html
 
-Ik Heb nu een film poster en de titel erboven vanuit de Json op de pagina gekregen.
+Ik Heb nu een filmposter en de titel erboven vanuit de Json op de pagina gekregen.
 
 ![html screen 1](docu3/2.jpg "versie 1")
 
-Nu heb ik met een array alle films met de titels eronder op de pagina gekregen. Het ging op zich wel vlot maar ik maakte hier wat foutjes waardoor alles een stuk moeilijker ging dan had gekunt maar het werkt nu!
+Nu heb ik met een array alle films met de titels eronder op de pagina gekregen. Het ging op zich wel vlot maar ik maakte hier wat foutjes waardoor alles een stuk moeilijker ging dan had gekund maar het werkt nu!
 
 ![html screen 2](docu3/3.jpg "versie 2")
 
@@ -38,7 +37,26 @@ Ik heb nu wel de button werkend gekregen in java script maar ik snap niet hoe je
 
 ![html screen 2](docu3/6.jpg "versie 6")
 
-Naast dat het menu in en uit het beeld moet schuiven wil ik van de lijst van films eigenlijk een carousel/gallery maken. Ik ben nog opzoek naar een makkelijke plug-in daarvoor maar ik kan zover alleen nog maar J-query plug ins vinden die ik niet begrijp, en anders moet ik het gewoon zelf coderen mischien even naar mijn opdracht 2 kijken daarvoor.
+Ik heb met behulp van de codepen van Sanne het in en uit schuif menu werkend gekregen zoals ik het wilde. Het was wel een hoop trial and error. Ik wilde hem uit het beeld laten schuiven zonder dat mijn viewport groter werd, dit is mij uiteindelijk gelukt door position fixed te gebruiken. Nu heb ik onderaan mijn website een film lijst die in en uit beeld komt schuiven.
+
+![html screen 2](docu3/7.jpg "versie 6")
+
+Nu ik het sortable gedeelte, en het menu af heb wil ik de carrousel gaan maken. Ik heb het eerst geprobeerd met de code van mijn opdracht 2. Maar ik kwam er niet helemaal uit. Ik moest meerdere ID's aanmaken dan voor "li" die vanuit de jason werden aangemaakt, ik kwam wel zover dat ik 1 ID kon afmaken maar alle "li" kregen dan die ID. Dus na veel klooien ben ik maar weer overnieuw begonnen.
+
+![html screen 2](docu3/9.jpg "versie 6")
+
+Ik heb heel wat tutorials gekeken op youtube, en meerdere tutorials online opgezocht maar veel van die tutorials gebruikte Jquery, en ik heb geen idee hoe dat werkt. Dus daarna ben ik naar Sanne's codepen gegaan en heb naar zijn soort van 3D carrousel codepen gekeken. Dankzij dit heb ik nu een werkende carousel met sortable samen of eigenlijk bijna werkend
+
+![html screen 2](docu3/10.jpg "versie 6")
+
+Het ding was hij werkte volledig, ik kon de images door elkaar husselen, ik kon ze in mijn uitklapmenu slepen, en ik kon naar links draaien en met de pijltjes en het pijltje op het scherm. Alleen ik kon dus niet naar rechts draaien. Zodra ik pijltje naar rechts drukte of op het rechter pijltje klikte stackde alle posters ineens op elkaar, en kon ik verder ook niet meer iets besturen. Ik had uiteindelijk een spelfout gemaakt bij de functie van het naar rechts gaan. Nadat ik dit had gefixed werkte hij volledig zoals ik het in eerste instantie had voorgesteld.
+
+Nu heb ik de website nog wat mooier gemaakt met wat hovers en kleuren en vind ik hem best wel goed gelukt.
+
+![html screen 2](docu3/12.jpg "versie 6")
+
+
+
 
 
 
--- opdracht3/js/js.js
+++ opdracht3/js/js.js
@@ -1,9 +1,10 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
 
-var filmlijst = document.querySelector('main');
+var filmlijst = document.querySelector('ul');
 var mijnlijst = document.querySelector('.myList');
-var button = document.querySelector('button');
+var button = document.querySelector('.listButton');
+var deSlider = document.querySelector('.slider');
+var radio_array = new Array();
+var positieSlider;
 
 
 let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
@@ -11,35 +12,41 @@ let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+
 request.onload = function () {
     const movies = request.response;
     showMovies(movies);
-    console.log(movies);
+    interactionSlider();
 }
-//
-//function showtitle(jsonObj) {
-//    const title = document.createElement('p');
-//    title.textContent = jsonObj['0']['title'];
-//    header.appendChild(title);
-//}
+
 
 function showMovies(movies) {
     for (let i = 0; i < movies.length; i++) {
 
-      let title = document.createElement('h2');
-      title.textContent = movies[i]['title'];
+        let film = document.createElement('li');
+        let poster = document.createElement('img');
+        let title = document.createElement('h2');
 
-      let poster = document.createElement('img');
-      poster.src = movies[i]['cover'];
+        poster.src = movies[i].cover;
+        poster.setAttribute("alt", movies[i].title);
+        title.innerHTML = movies[i].title;
 
-      let film = document.createElement('article');
-      film.appendChild(title);
-      film.appendChild(poster);
 
-      document.body.querySelector("main").appendChild(film);
+
+        film.appendChild(title);
+        film.appendChild(poster);
+
+        document.body.querySelector("ul").appendChild(film);
+
+        deSlider.appendChild(film);
+        positieSlider = 0;
     }
+
+
+
 }
 
+//Sortable code.
 
 new Sortable(filmlijst, {
     group: 'shared', // set both lists to same group
@@ -51,10 +58,70 @@ new Sortable(mijnlijst, {
     animation: 150
 });
 
-
-button.addEventListener('click', function (event) {
-  alert('en dan wil ik dat dit lijstje omhoog schuift vanuit het beeld, maar dat krijg ik niet aan de praat.');
+button.addEventListener("click", function () {
+    document.body.classList.toggle("beweeg");
 });
 
 
+//hieronder de carousel code met behulp van de codepen van Sanne.
 
+function interactionSlider() {
+    var buttonRight = document.getElementById("right");
+    var buttonLeft = document.getElementById("left");
+
+    buttonLeft.addEventListener("click", function () {
+        moveSlider("naarLinks");
+    });
+    buttonRight.addEventListener("click", function () {
+        moveSlider("naarRechts");
+    });
+
+    window.addEventListener("keydown", function () {
+        if (event.key == "ArrowLeft") {
+            moveSlider("naarLinks");
+        } else if (event.key == "ArrowRight") {
+            moveSlider("naarRechts");
+        }
+    });
+    deSlider.addEventListener("change", function (event) {
+        var deRadioButton = event.target;
+        var deRadioButtonID = deRadioButton.id;
+        var deRadioButtonIDnummer = deRadioButtonID.substring(5);
+        var newPositieSlider = parseInt(deRadioButtonIDnummer) - 1;
+        positieSlider = newPositieSlider;
+    });
+
+}
+
+function moveSlider(richting) {
+    var radio_length = radio_array.length;
+
+    if (richting == "naarLinks") {
+        draaiCarrousel("naarLinks");
+
+    } else if (richting == "naarRechts") {+
+        draaiCarrousel("naarRechts");
+    }
+}
+
+
+
+function draaiCarrousel(richting) {
+
+    var deCarrousel = document.querySelector(".slider");
+    var huidigeCarrouselHoek;
+    var nieuweCarrouselHoek;
+
+    huidigeCarrouselHoek = getComputedStyle(deCarrousel).getPropertyValue("--carrouselHoek");
+    huidigeCarrouselHoek = huidigeCarrouselHoek.substring(0, huidigeCarrouselHoek.length - 3);
+    huidigeCarrouselHoek = parseInt(huidigeCarrouselHoek);
+
+    if (richting == "naarLinks") {
+        nieuweCarrouselHoek = huidigeCarrouselHoek - 60;
+
+    } else if (richting == "naarRechts") {
+        nieuweCarrouselHoek = huidigeCarrouselHoek + 60;
+    }
+    nieuweCarrouselHoek = nieuweCarrouselHoek + "deg";
+    deCarrousel.style.setProperty("--carrouselHoek", nieuweCarrouselHoek);
+}
